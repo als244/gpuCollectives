b34467db1e332f26e1cf3a7a30ef4954b00f357c
aceaton <aceaton@princeton.edu>
2023-12-12 16:27:43 -0500

    handle status

--- amd_test/amd_dummy.cpp
+++ amd_test/amd_dummy.cpp
@@ -4,9 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #define CHECK_HIP_ERROR(error)                    \
-    if(error != hipSuccess)                       \
+    if (error != hipSuccess)                      \
     {                                             \
         fprintf(stderr,                           \
                 "hip error: '%s'(%d) at %s:%d\n", \
@@ -18,7 +17,7 @@
     }
 
 #define CHECK_ROCBLAS_STATUS(status)                  \
-    if(status != rocblas_status_success)              \
+    if (status != rocblas_status_success)             \
     {                                                 \
         fprintf(stderr, "rocBLAS error: ");           \
         fprintf(stderr,                               \
@@ -30,11 +29,13 @@
         exit(EXIT_FAILURE);                           \
     }
 
-void sampleNormal(float * X, int size, float mean, float var) {
+void sampleNormal(float *X, int size, float mean, float var)
+{
     float x, y, z, std, val;
-    for (int i = 0; i < size; i++){
-        x = (float)rand() / (float) RAND_MAX;
-        y = (float)rand() / (float) RAND_MAX;
+    for (int i = 0; i < size; i++)
+    {
+        x = (float)rand() / (float)RAND_MAX;
+        y = (float)rand() / (float)RAND_MAX;
         z = sqrtf(-2 * logf(x)) * cosf(2 * M_PI * y);
         std = sqrtf(var);
         val = std * z + mean;
@@ -42,10 +43,9 @@ void sampleNormal(float * X, int size, float mean, float var) {
     }
 }
 
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    
+
     rocblas_status rstatus = rocblas_status_success;
 
     typedef float dataType;
@@ -55,77 +55,81 @@ int main(int argc, char** argv)
     rocblas_int K = 8192;
 
     float hAlpha = 1;
-    float hBeta  = 0;
+    float hBeta = 0;
 
     const rocblas_operation transA = rocblas_operation_none;
     const rocblas_operation transB = rocblas_operation_none;
 
     rocblas_int lda, ldb, ldc, sizeA, sizeB, sizeC;
-    int         strideA1, strideA2, strideB1, strideB2;
+    int strideA1, strideA2, strideB1, strideB2;
 
-    if(transA == rocblas_operation_none)
+    if (transA == rocblas_operation_none)
     {
-        lda      = M;
-        sizeA    = K * lda;
+        lda = M;
+        sizeA = K * lda;
         strideA1 = 1;
         strideA2 = lda;
     }
     else
     {
-        lda      = K;
-        sizeA    = M * lda;
+        lda = K;
+        sizeA = M * lda;
         strideA1 = lda;
         strideA2 = 1;
     }
-    if(transB == rocblas_operation_none)
+    if (transB == rocblas_operation_none)
     {
-        ldb      = K;
-        sizeB    = N * ldb;
+        ldb = K;
+        sizeB = N * ldb;
         strideB1 = 1;
         strideB2 = ldb;
     }
     else
     {
-        ldb      = N;
-        sizeB    = K * ldb;
+        ldb = N;
+        sizeB = K * ldb;
         strideB1 = ldb;
         strideB2 = 1;
     }
 
-    ldc   = M;
+    ldc = M;
     sizeC = N * ldc;
 
     // using rocblas API
     rocblas_handle handle;
     rstatus = rocblas_create_handle(&handle);
+    if (rstatus != rocblas_status_success)
+    {
+        fprintf(stderr, "Error creating ROCBLAS handle: %s\n", rocblas_status_to_string(rstatus));
+        // Handle the error or exit the program
+    }
+
     CHECK_ROCBLAS_STATUS(rstatus);
 
     // Naming: dX is in GPU (device) memory. hK is in CPU (host) memory
-    float * hA, *hB, *hC;
-    hA = (float *) malloc(sizeA * sizeof(float));
-    hB = (float *) malloc(sizeB * sizeof(float));
-    hC = (float *) malloc(sizeC * sizeof(float));
+    float *hA, *hB, *hC;
+    hA = (float *)malloc(sizeA * sizeof(float));
+    hB = (float *)malloc(sizeB * sizeof(float));
+    hC = (float *)malloc(sizeC * sizeof(float));
 
     sampleNormal(hA, sizeA, 0, 1);
     sampleNormal(hB, sizeB, 0, 1);
 
-
     // allocate memory on device
-    float * dA, *dB, *dC;
+    float *dA, *dB, *dC;
     hipMalloc(&dA, sizeA * sizeof(float));
     hipMalloc(&dB, sizeB * sizeof(float));
     hipMalloc(&dC, sizeC * sizeof(float));
-    
-    if(!dA || !dB || !dC)
+
+    if (!dA || !dB || !dC)
     {
         CHECK_HIP_ERROR(hipErrorOutOfMemory);
         return EXIT_FAILURE;
     }
 
-     // copy data from CPU to device
+    // copy data from CPU to device
     CHECK_HIP_ERROR(hipMemcpy(dA, hA, sizeof(float) * sizeA, hipMemcpyHostToDevice));
     CHECK_HIP_ERROR(hipMemcpy(dB, hB, sizeof(float) * sizeB, hipMemcpyHostToDevice));
-
 
     // enable passing alpha parameter from pointer to host memory
     rstatus = rocblas_set_pointer_mode(handle, rocblas_pointer_mode_host);
