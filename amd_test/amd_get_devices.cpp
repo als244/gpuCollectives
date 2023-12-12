078b3e81722301a564f269d82a4fe81bfb220236
aceaton <aceaton@princeton.edu>
2023-12-12 16:48:34 -0500

    afsd

--- amd_test/amd_get_devices.cpp
+++ amd_test/amd_get_devices.cpp
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
     int ndevs = 0;
     CHECK_HIP_ERROR(hipGetDeviceCount(&ndevs));
 
-    devProp = hipGetDeviceProperties();
+    hipGetDeviceProperties(&devProp, 0);
     for (int i = 0; i < ndevs; i++)
     {
         CHECK_HIP_ERROR(hipGetDeviceCount(&devProp, i));
